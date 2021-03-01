0d8c417fd818028c0da250dede2587fde5384de3
itelligent-dmariscal <55440841+itelligent-dmariscal@users.noreply.github.com>
2021-03-01 16:26:00 +0200

    Segunda versión del prototipo

---
+++ Prototipo2/opra.itelligent.es/opra.itelligent.es/Services/MenuService.cs
@@ -0,0 +1,45 @@
+﻿using Itelligent.Web.Ui.Models;
+using Itelligent.Web.Ui.Services;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace opra.itelligent.es.Services
+{
+    public class MenuService : IMenuService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public MenuService(IHttpContextAccessor httpContextAcessor)
+        {
+            _httpContextAccessor = httpContextAcessor;
+        }
+
+        public MenuItem GetHome()
+        {
+            return new MenuItem("Inicio", "Home", "Index");
+        }
+
+        public IEnumerable<MenuItem> GetMenu()
+        {
+            List<MenuItem> model = new List<MenuItem>
+            {
+                new MenuItem("Inicio", "Home", "Index"),
+                new MenuItem("Noticias", "News", "Index"),
+                new MenuItem("Repositorio", urlExterna: "https://github.com/ITelligent-Information-Technologies/opra"),
+                new MenuItem("Área privada", "PrivateArea", "Index")
+                {
+                    HtmlExtra = async () =>
+                    {
+                        await Task.Yield();
+                        return _httpContextAccessor.HttpContext.User.Identity.IsAuthenticated ? "" : "<i class='fas fa-lock ml-2'></i>";
+                    }
+                }
+            };
+
+            return model;
+        }
+    }
+}
---
+++ Prototipo2/opra.itelligent.es/opra.itelligent.es/Services/ProblemasService.cs
@@ -0,0 +1,89 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using ITelligent.Scheduling;
+using Microsoft.Extensions.Caching.Memory;
+using Newtonsoft.Json;
+using opra.itelligent.es.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace opra.itelligent.es.Services
+{
+    public class ProblemasService
+    {
+        private readonly IAmazonS3 _s3Client;
+        private readonly IMemoryCache _cache;
+        private readonly BdOpraContext _context;
+
+        public ProblemasService(IAmazonS3 s3Client, IMemoryCache cache, BdOpraContext context)
+        {
+            _s3Client = s3Client;
+            _cache = cache;
+            _context = context;
+        }
+
+        public Task<clsDatosJobShop> GetDatosProblema(int problema)
+        {
+           return _cache.GetOrCreateAsync(problema, async entry =>
+            {
+                TblMaestraProblema datosProblema = _context.TblMaestraProblema.FirstOrDefault(x => x.IntId == problema);
+
+                using (GetObjectResponse responseS3 = await _s3Client.GetObjectAsync(datosProblema.StrBucketS3, datosProblema.StrKeyS3))
+                using (Stream stream = responseS3.ResponseStream)
+                using (StreamReader reader = new StreamReader(stream))
+                {
+                    return JsonConvert.DeserializeObject<clsDatosJobShop>(reader.ReadToEnd());
+                }
+            });
+        }
+
+        public clsDatosSchedule GetPlanificacion(string guid)
+        {
+            return _cache.Get<clsDatosSchedule>($"schedule_{guid}");
+        }
+
+        public clsDatosSchedule GuardarPlanificacion(clsDatosJobShop datosJobShop, string guid)
+        {
+            return _cache.GetOrCreate($"schedule_{guid}", entry =>
+            {
+                clsSolucionInicial cSInicial = new clsSolucionInicial();
+                clsDatosSchedule cSchedule = cSInicial.OrdenarPorLongitudTrabajos(datosJobShop);
+
+                // Calcula el bellman para la solucion actual
+                clsBellman cBellman = new clsBellman();
+                cBellman.CalcularBellman(datosJobShop, cSchedule);
+
+                return cSchedule;
+            });
+        }
+
+        public void GenerarHorario(clsDatosResultados cResultados, clsDatosSchedule cSchedule, DateTime dtmStartDate)
+        {
+            clsDatosHorarios cDatosHorarios = new clsDatosHorarios();
+            List<clsDatosHorariosHoras> lstHoras = new List<clsDatosHorariosHoras>();
+            clsDatosHorariosHoras cHoras = new clsDatosHorariosHoras();
+            cHoras.dblHoraDesde = 9;
+            cHoras.dblHoraHasta = 17;
+            lstHoras.Add(cHoras);
+            cDatosHorarios.dicIdDiasSemanaHorarios.Add(1, lstHoras);
+            cDatosHorarios.dicIdDiasSemanaHorarios.Add(2, lstHoras);
+            cDatosHorarios.dicIdDiasSemanaHorarios.Add(3, lstHoras);
+            cDatosHorarios.dicIdDiasSemanaHorarios.Add(4, lstHoras);
+            cDatosHorarios.dicIdDiasSemanaHorarios.Add(5, lstHoras);
+            clsDatosHorariosHoras cHorariosFiesta = new clsDatosHorariosHoras();
+            cHorariosFiesta.blnSinActividad = true;
+            List<clsDatosHorariosHoras> lstHorasFinSemana = new List<clsDatosHorariosHoras>();
+            lstHorasFinSemana.Add(cHorariosFiesta);
+            cDatosHorarios.dicIdDiasSemanaHorarios.Add(6, lstHorasFinSemana);
+            cDatosHorarios.dicIdDiasSemanaHorarios.Add(7, lstHorasFinSemana);
+            // Mete un festivo
+            cDatosHorarios.dicFechasEspecialesHorarios.Add(Convert.ToDateTime("02/01/2020"), lstHorasFinSemana);
+
+            clsHorarios cHorarios = new clsHorarios();
+            (cResultados.dicIdOperacionFechaStartEnd, cResultados.dicIdOperacionListaStartEnd) = cHorarios.ObtenerFechas(cDatosHorarios, cResultados.cData, cSchedule, dtmStartDate);
+        }
+    }
+}
