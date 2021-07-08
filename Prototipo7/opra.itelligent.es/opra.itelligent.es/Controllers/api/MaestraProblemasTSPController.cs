ff1947cbfee1a3b1a9310dc094eeee0cc94a61b0
itelligent-dmariscal <55440841+itelligent-dmariscal@users.noreply.github.com>
2021-07-08 16:42:00 +0200

    Séptima Versión del Prototipo

---
+++ Prototipo7/opra.itelligent.es/opra.itelligent.es/Controllers/api/MaestraProblemasTSPController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using opra.itelligent.es.Models;
+using opra.itelligent.es.Services;
+using opra.itelligent.es.ViewModels;
+
+namespace opra.itelligent.es.Controllers.api
+{
+    [Authorize]
+    public class MaestraProblemasTSPController : ODataController
+    {
+        private readonly BdOpraContext _context;
+        private readonly VehicleRoutingService _vehicleRoutingService;
+
+        public MaestraProblemasTSPController(BdOpraContext context, VehicleRoutingService vehicleRoutingService)
+        {
+            _context = context;
+            _vehicleRoutingService = vehicleRoutingService;
+        }
+
+        // GET: api/MaestraProblemas
+        [HttpGet]
+        [EnableQuery]
+        public IQueryable<TblMaestraProblemaTsp> Get()
+        {
+            return _context.TblMaestraProblemaTsp;
+        }
+
+        // GET: api/MaestraProblemas/5
+        [EnableQuery]
+        public async Task<ActionResult<TblMaestraProblemaTsp>> GetTblMaestraProblemaTsp(int key)
+        {
+            var TblMaestraProblemaTsp = await _context.TblMaestraProblemaTsp.FindAsync(key);
+
+            if (TblMaestraProblemaTsp == null)
+            {
+                return NotFound();
+            }
+
+            return TblMaestraProblemaTsp;
+        }
+
+        // PUT: api/MaestraProblemas/5
+        public async Task<IActionResult> Put([FromODataUri] int key, Delta<TblMaestraProblemaTsp> patch)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            TblMaestraProblemaTsp TblMaestraProblemaTsp = _context.TblMaestraProblemaTsp.Find(key);
+            if (TblMaestraProblemaTsp == null)
+            {
+                return NotFound();
+            }
+
+            patch.Put(TblMaestraProblemaTsp);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TblMaestraProblemaTspExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(TblMaestraProblemaTsp);
+        }
+        
+
+        // POST: api/MaestraProblemas
+        [HttpPost]
+        [EnableQuery]
+        public async Task<IActionResult> PostTblMaestraProblemaTsp([FromBody]TblMaestraProblemaTsp TblMaestraProblemaTsp)
+        {
+            _context.TblMaestraProblemaTsp.Add(TblMaestraProblemaTsp);
+            await _context.SaveChangesAsync();
+
+            return Created(TblMaestraProblemaTsp);
+        }
+
+        [AcceptVerbs("PATCH", "MERGE")]
+        public async Task<IActionResult> Patch([FromODataUri] int key, Delta<TblMaestraProblemaTsp> patch)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            TblMaestraProblemaTsp TblMaestraProblemaTsp = await _context.TblMaestraProblemaTsp.FindAsync(key);
+            if (TblMaestraProblemaTsp == null)
+            {
+                return NotFound();
+            }
+
+            patch.Patch(TblMaestraProblemaTsp);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TblMaestraProblemaTspExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(TblMaestraProblemaTsp);
+        }
+
+        public async Task<IActionResult> Delete([FromODataUri] int key)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var TblMaestraProblemaTsp = await _context.TblMaestraProblemaTsp.FindAsync(key);
+            if (TblMaestraProblemaTsp == null)
+            {
+                return NotFound();
+            }
+
+            _context.TblMaestraProblemaTsp.Remove(TblMaestraProblemaTsp);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        private bool TblMaestraProblemaTspExists(int id)
+        {
+            return _context.TblMaestraProblemaTsp.Any(e => e.IntId == id);
+        }
+
+        public async Task<List<PointData>> CoordenadasOptimas([FromODataUri] int key)
+        {
+            List<PointData> coordOptimas = await _vehicleRoutingService.ObtenerCoordenadasOptimas(key);
+
+            return coordOptimas;
+        }
+    }
+}
---
+++ Prototipo7/opra.itelligent.es/opra.itelligent.es/Util/SchedulerUtil.cs
@@ -0,0 +1,103 @@
+﻿using ITelligent.Scheduling;
+using netIndustrial.Models;
+using opra.itelligent.es.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace opra.itelligent.es.Util
+{
+    public class SchedulerUtil
+    {
+        public static RespuestaScheduler GenerarRespuesta(int problema, string guid, clsDatosResultados cResultados, IEnumerable<int> lstIdOperationInCriticalPath, DateTime dtmStarDate, double dblMakespan)
+        {
+            RespuestaScheduler resp = new RespuestaScheduler();
+
+            List<EventData> lstOperacionesPorMaquina = new List<EventData>();
+            List<EventData> lstOperacionesPorJob = new List<EventData>();
+            Dictionary<int, ResourceData> dicMaquinas = new Dictionary<int, ResourceData>();
+            Dictionary<int, ResourceData> dicJobs = new Dictionary<int, ResourceData>();
+            DateTime dtmEndDate = cResultados.dicIdOperacionFechaStartEnd.Values.Max(x => x.dtmFechaEnd);
+
+            foreach (var operacion in cResultados.cData.dicIdOperationIdMachine)
+            {
+                int intIdOperacion = operacion.Key;
+                int intIdMachine = operacion.Value;
+                int intIdJob = cResultados.cData.dicIdOperationIdJob[intIdOperacion];
+
+                if (!dicMaquinas.ContainsKey(intIdMachine))
+                {
+                    dicMaquinas.Add(intIdMachine, new ResourceData()
+                    {
+                        orden = intIdMachine,
+                        id = intIdMachine.ToString(),
+                        name = "Máquina " + intIdMachine
+                    });
+                }
+
+                if (!dicJobs.ContainsKey(intIdJob))
+                {
+                    dicJobs.Add(intIdJob, new ResourceData()
+                    {
+                        orden = intIdJob,
+                        id = intIdJob.ToString(),
+                        name = "JOB " + intIdJob
+                    });
+                }
+
+                foreach (clsFechasStartEnd bloque in cResultados.dicIdOperacionListaStartEnd[intIdOperacion])
+                {
+                    EventData eventoMaquina = new EventData()
+                    {
+                        id = intIdOperacion.ToString(),
+                        id2 = intIdJob.ToString(),
+                        resource = intIdMachine.ToString(),
+                        startDate = bloque.dtmFechaStart,
+                        endDate = bloque.dtmFechaEnd,
+                        text = "J - " + intIdJob + " Operacion - " + intIdOperacion
+
+                    };
+                    eventoMaquina.bubbleHtml = $"<H6>Operación {intIdOperacion} </H6><ul><li><b>Job:</b> {intIdJob}</li><li><b>Fecha Inicio:</b> {eventoMaquina.startDate.ToString("dd/MM/yyyy HH:mm")}</li><li><b>Fecha Fin:</b>{eventoMaquina.endDate.ToString("dd/MM/yyyy HH:mm")}</li></lu>";
+
+                    EventData eventoJob = new EventData()
+                    {
+                        id = intIdOperacion.ToString(),
+                        id2 = intIdMachine.ToString(),
+                        resource = intIdJob.ToString(),
+                        startDate = bloque.dtmFechaStart,
+                        endDate = bloque.dtmFechaEnd,
+                        text = "M - " + intIdMachine.ToString() + " Operación - " + intIdOperacion,
+
+                    };
+                    eventoJob.bubbleHtml = $"<H6>Operación {intIdOperacion} </H6><ul><li><b>Maquina:</b> {intIdMachine}</li><li><b>Fecha Inicio:</b> {eventoJob.startDate.ToString("dd/MM/yyyy HH:mm")}</li><li><b>Fecha Fin:</b>{eventoJob.endDate.ToString("dd/MM/yyyy HH:mm")}</li></lu>";
+
+                    if (lstIdOperationInCriticalPath.Contains(intIdOperacion))
+                    {
+                        eventoMaquina.borderColor = eventoJob.borderColor = "red";
+                    }
+
+                    lstOperacionesPorMaquina.Add(eventoMaquina);
+                    lstOperacionesPorJob.Add(eventoJob);
+                }
+            }
+
+            List<ResourceData> lstMaquinas = dicMaquinas.Values.OrderBy(x => x.orden).ToList();
+            List<ResourceData> lstJobs = dicJobs.Values.OrderBy(x => x.orden).ToList();
+
+            resp.IdProblema = problema;
+            resp.Guid = guid;
+            resp.StartDate = dtmStarDate;
+            resp.Days = Math.Ceiling((dtmEndDate - dtmStarDate).TotalDays);
+            resp.EventsMachine = lstOperacionesPorMaquina;
+            resp.EventsJob = lstOperacionesPorJob;
+            resp.ResourcesMachine = lstMaquinas;
+            resp.ResourcesJob = lstJobs;
+            resp.BestMakespan = cResultados.cData.dblMakespanBest;
+
+            resp.Makespan = dblMakespan;
+
+            return resp;
+        }
+    }
+}
