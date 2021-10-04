16b400a543f729c2acf8f1d244a48c44f3636fda
itelligent-dmariscal <55440841+itelligent-dmariscal@users.noreply.github.com>
2021-10-04 16:49:00 +0200

    Novena Versión del Prototipo

---
+++ Prototipo9/opra.itelligent.es/opra.itelligent.es/Controllers/api/SolucionesTSPController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using opra.itelligent.es.Models;
+
+namespace opra.itelligent.es.Controllers.api
+{
+    [Authorize]
+    public class SolucionesTSPController : ODataController
+    {
+        private readonly BdOpraContext _context;
+
+        public SolucionesTSPController(BdOpraContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [EnableQuery]
+        public IQueryable<TblSolucionTsp> Get()
+        {
+            return _context.TblSolucionTsp;
+        }
+
+        [EnableQuery]
+        public async Task<ActionResult<TblSolucionTsp>> GetTblSolucionTsp(int key)
+        {
+            var TblSolucionTsp = await _context.TblSolucionTsp.FindAsync(key);
+
+            if (TblSolucionTsp == null)
+            {
+                return NotFound();
+            }
+
+            return TblSolucionTsp;
+        }
+
+        public async Task<IActionResult> Put([FromODataUri] int key, Delta<TblSolucionTsp> patch)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            TblSolucionTsp TblSolucionTsp = _context.TblSolucionTsp.Find(key);
+            if (TblSolucionTsp == null)
+            {
+                return NotFound();
+            }
+
+            patch.Put(TblSolucionTsp);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TblSolucionTspExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(TblSolucionTsp);
+        }
+        
+
+        [HttpPost]
+        [EnableQuery]
+        public async Task<IActionResult> PostTblSolucionTsp([FromBody]TblSolucionTsp TblSolucionTsp)
+        {
+            _context.TblSolucionTsp.Add(TblSolucionTsp);
+            await _context.SaveChangesAsync();
+
+            return Created(TblSolucionTsp);
+        }
+
+        [AcceptVerbs("PATCH", "MERGE")]
+        public async Task<IActionResult> Patch([FromODataUri] int key, Delta<TblSolucionTsp> patch)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            TblSolucionTsp TblSolucionTsp = await _context.TblSolucionTsp.FindAsync(key);
+            if (TblSolucionTsp == null)
+            {
+                return NotFound();
+            }
+
+            patch.Patch(TblSolucionTsp);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TblSolucionTspExists(key))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Updated(TblSolucionTsp);
+        }
+
+        public async Task<IActionResult> Delete([FromODataUri] int key)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var TblSolucionTsp = await _context.TblSolucionTsp.FindAsync(key);
+            if (TblSolucionTsp == null)
+            {
+                return NotFound();
+            }
+
+            _context.TblSolucionTsp.Remove(TblSolucionTsp);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        private bool TblSolucionTspExists(int id)
+        {
+            return _context.TblSolucionTsp.Any(e => e.IntId == id);
+        }
+    }
+}
---
+++ Prototipo9/opra.itelligent.es/opra.itelligent.es/Services/VehicleRoutingService.cs
@@ -0,0 +1,95 @@
+﻿using Amazon.S3;
+using Amazon.S3.Model;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using opra.itelligent.es.Models;
+using opra.itelligent.es.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace opra.itelligent.es.Services
+{
+    public class VehicleRoutingService
+    {
+        private readonly IConfiguration _conf;
+        private readonly BdOpraContext _context;
+        private readonly IAmazonS3 _s3Client;
+
+        public VehicleRoutingService(IConfiguration conf, BdOpraContext context, IAmazonS3 s3Client)
+        {
+            _conf = conf;
+            _context = context;
+            _s3Client = s3Client;
+        }
+
+        public async Task<List<PointData>> ObtenerCoordenadas(int idProblema)
+        {
+            TblMaestraProblemaTsp datosProblema = _context.TblMaestraProblemaTsp.FirstOrDefault(x => x.IntId == idProblema);
+
+            return await ObtenerCoordenadas(datosProblema);
+        }
+
+        public async Task<List<PointData>> ObtenerCoordenadas(TblMaestraProblemaTsp datosProblema)
+        {
+            double bottomLeftLng = _conf.GetValue<double>("Coordenadas:bottomLeftLng");
+            double bottomLeftLat = _conf.GetValue<double>("Coordenadas:bottomLeftLat");
+            double upperRightLng = _conf.GetValue<double>("Coordenadas:upperRightLng");
+            double upperRightLat = _conf.GetValue<double>("Coordenadas:upperRightLat");
+
+            double distanceX = Math.Abs(upperRightLng - bottomLeftLng);
+            double distanceY = Math.Abs(upperRightLat - bottomLeftLat);
+
+            using (GetObjectResponse responseS3 = await _s3Client.GetObjectAsync(datosProblema.StrBucketS3, datosProblema.StrKeyS3))
+            using (Stream stream = responseS3.ResponseStream)
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string[] data = reader.ReadToEnd().Split("output");
+                string[] rawPoints = data[0].Split(' ', System.StringSplitOptions.RemoveEmptyEntries);
+
+                List<PointData> puntos = new List<PointData>();
+
+                for (int i = 0; i < rawPoints.Length; i += 2)
+                {
+                    int index = i / 2;
+                    double x = double.Parse(rawPoints[i], CultureInfo.InvariantCulture);
+                    double y = double.Parse(rawPoints[i + 1], CultureInfo.InvariantCulture);
+
+                    PointData pd = new PointData
+                    {
+                        nombre = $"Punto {index + 1}",
+                        lng = bottomLeftLng + distanceX * x,
+                        lat = bottomLeftLat + distanceY * y,
+                        x = x,
+                        y = y
+                    };
+
+                    puntos.Add(pd);
+                }
+
+                return puntos;
+            }
+        }
+
+        public async Task<List<PointData>> ObtenerCoordenadasOptimas(int idProblema)
+        {
+            TblMaestraProblemaTsp datosProblema = _context.TblMaestraProblemaTsp.FirstOrDefault(x => x.IntId == idProblema);
+            List<int> ordenOptimo = JsonConvert.DeserializeObject<List<int>>(datosProblema.StrSolucionOptima);
+            ordenOptimo.Add(ordenOptimo[0]);
+
+            List<PointData> puntos = await ObtenerCoordenadas(datosProblema);
+
+            List<PointData> puntosOrdenados = new List<PointData>();
+
+            for (int i = 0; i < ordenOptimo.Count; i++)
+            {
+                puntosOrdenados.Add(puntos[ordenOptimo[i]]);
+            }
+
+            return puntosOrdenados;
+        }
+    }
+}
