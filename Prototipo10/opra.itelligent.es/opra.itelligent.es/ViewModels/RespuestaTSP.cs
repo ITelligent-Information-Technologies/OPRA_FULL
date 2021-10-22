65c6f0cc8adb8b2c8198e94b66277361d1f63276
itelligent-dmariscal <55440841+itelligent-dmariscal@users.noreply.github.com>
2021-10-22 16:39:00 +0200

    Décima Versión del Prototipo

---
+++ Prototipo10/opra.itelligent.es/opra.itelligent.es/ViewModels/RespuestaTSP.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace opra.itelligent.es.ViewModels
+{
+    public class RespuestaTSP
+    {
+        public IEnumerable<PointData> Puntos { get; set; }
+        public double Coste { get; set; }
+        public double Tiempo { get; set; }
+    }
+}
