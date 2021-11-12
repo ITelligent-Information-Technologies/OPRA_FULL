b667b9c835aefa9fb185160d65ab459937661999
ITelligent-JoseAntonio <jacaravaca@itelligent.es>
2021-11-12 12:29:00 +0200

    Undécima versión del prototipo

---
+++ Prototipo11/opra.itelligent.es/opra.itelligent.es/Controllers/SchedulerController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.S3;
+using Amazon.S3.Model;
+using ITelligent.Scheduling;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+using netIndustrial.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using opra.itelligent.es.Models;
+using opra.itelligent.es.Services;
+using opra.itelligent.es.Util;
+
+namespace opra.itelligent.es.Controllers
+{
+    public class SchedulerController : Controller
+    {
+        private readonly ProblemasService _problemasService;
+
+        public SchedulerController(ProblemasService problemasService)
+        {
+            _problemasService = problemasService;
+        }
+
+        public async Task<IActionResult> Index(int problema)
+        {
+            clsDatosJobShop datosJobShop = await _problemasService.GetDatosProblema(problema);
+
+            string guid = Guid.NewGuid().ToString();
+
+            clsDatosSchedule cSchedule = _problemasService.GuardarPlanificacion(datosJobShop, guid);
+
+            clsDatosResultados cResultados = new clsDatosResultados()
+            {
+                cData = datosJobShop
+            };
+
+            DateTime dtmStartDate = DateTime.Today;
+
+            // Pruba a genera el schedule con un horario
+            _problemasService.GenerarHorario(cResultados, cSchedule, dtmStartDate);
+           
+            RespuestaScheduler resp = SchedulerUtil.GenerarRespuesta(problema, guid, cResultados, cSchedule.hsIdOperationInCriticalPath, dtmStartDate, cSchedule.dblMakespan);
+
+            return View(resp);
+        }
+    }
+}
---
+++ Prototipo11/opra.itelligent.es/opra.itelligent.es/Controllers/api/VehicleRoutingApiController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using opra.itelligent.es.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace opra.itelligent.es.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class VehicleRoutingApiController : ControllerBase
+    {
+        [HttpPost("optimizar/{modelo}")]
+        public async Task<IActionResult> Optimizar(int modelo, [FromBody] IEnumerable<PointData> datos)
+        {
+            //TODO
+            await Task.CompletedTask;
+
+            RespuestaTSP response = new RespuestaTSP
+            {
+                 Puntos = datos,
+                 Coste = new Random().NextDouble() * 10,
+                 Tiempo = new Random().NextDouble() * 2
+            };
+
+            return Ok(response);
+        }
+    }
+}
