d595685a1d151f81d94889b912905a2ab5fd7282
ITelligent-JoseAntonio <jacaravaca@itelligent.es>
2021-11-30 12:31:00 +0200

    Décimo tercera versión del prototipo

---
+++ Prototipo13/opra.itelligent.es/opra.itelligent.es/ViewModels/ResourceData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace netIndustrial.Models
+{
+    public class ResourceData
+    {
+        public int orden { get; set; }
+        public String id { get; set; }
+        public String name { get; set; }
+    }
+}
